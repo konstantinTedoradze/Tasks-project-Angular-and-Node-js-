aed47d0bf7fbf08133b5926dc303ff1429a26785
konstantinTedoradze <97909996+konstantinTedoradze@users.noreply.github.com>
2022-02-01 12:29:43 +0200

    slq file

---
+++ mytasksList.sql
@@ -0,0 +1,81 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: mytasks
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `familymembers`
+--
+
+DROP TABLE IF EXISTS `familymembers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `familymembers` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `nick_name` varchar(45) NOT NULL,
+  `description` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `familymembers`
+--
+
+LOCK TABLES `familymembers` WRITE;
+/*!40000 ALTER TABLE `familymembers` DISABLE KEYS */;
+INSERT INTO `familymembers` VALUES (1,'konstantin','kost','father'),(2,'rusudan','ros','mother'),(3,'emily','emy','daughter');
+/*!40000 ALTER TABLE `familymembers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `taskslist`
+--
+
+DROP TABLE IF EXISTS `taskslist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `taskslist` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `family_id` int NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `creation_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `family_id` (`family_id`),
+  CONSTRAINT `taskslist_ibfk_1` FOREIGN KEY (`family_id`) REFERENCES `familymembers` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `taskslist`
+--
+
+LOCK TABLES `taskslist` WRITE;
+/*!40000 ALTER TABLE `taskslist` DISABLE KEYS */;
+INSERT INTO `taskslist` VALUES (1,1,'go to shoping','2021-09-05 09:05:47'),(2,2,'take out the dogs','2021-09-05 09:05:47'),(3,1,'take out the trash','2021-09-05 09:05:47'),(4,2,'go home','2021-09-05 09:05:55'),(6,1,'something something','2021-09-05 09:05:55');
+/*!40000 ALTER TABLE `taskslist` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-09-05 12:53:49
